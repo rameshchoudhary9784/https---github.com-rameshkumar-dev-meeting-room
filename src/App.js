9d5842c117d4ce9265df7408e3cb83c4a300df7b
rameshk978433@gmail.com <rameshk978433@gmail.com>
2024-08-14 07:07:03 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import ViewRoom from './components/ViewRoom';
+import SearchRoom from './components/SearchRoom';
+import BookRoom from './components/BookRoom';
+import CreateRoom from './components/CreateRoom';
 
 function App() {
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/view" element={<ViewRoom />} />
+        <Route path="/search" element={<SearchRoom />} />
+        <Route path="/book" element={<BookRoom />} />
+        <Route path="/create" element={<CreateRoom />} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
